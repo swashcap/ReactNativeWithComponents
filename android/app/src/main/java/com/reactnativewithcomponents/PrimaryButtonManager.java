461df9fdc3a642bcfa84e8391cf128a1436f4e1e
Cory Reed <swashcap@gmail.com>
2017-11-16 17:12:06 -0800

    fix(android): give button a layout

--- android/app/src/main/java/com/reactnativewithcomponents/PrimaryButtonManager.java
+++ android/app/src/main/java/com/reactnativewithcomponents/PrimaryButtonManager.java
@@ -6,6 +6,7 @@ import com.facebook.react.uimanager.annotations.ReactProp;
 import android.support.annotation.Nullable;
 
 import android.widget.Button;
+import android.view.ViewGroup.LayoutParams;
 
 public class PrimaryButtonManager extends SimpleViewManager<Button> {
   public static final String REACT_CLASS = "RNTPrimaryButton"; // Matches Objective-C exposed class
@@ -18,13 +19,14 @@ public class PrimaryButtonManager extends SimpleViewManager<Button> {
   @Override
   public Button createViewInstance(ThemedReactContext context) {
     Button button = new Button();
+    button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
     button.setText("Sample text!");
 
     return button;
   }
 
   @ReactProp(name = "title")
-  public void setTitle(Button view, @Nullable String title) {
-    view.setTitle(title);
+  public void setTitle(Button button, @Nullable String title) {
+    button.setTitle(title);
   }
 }
