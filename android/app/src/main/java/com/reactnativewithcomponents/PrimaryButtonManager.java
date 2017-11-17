727e93868cc17c07d15c83d47eefc836f7359f4d
Cory Reed <swashcap@gmail.com>
2017-11-17 12:08:51 -0800

    fix(android): correct button's height

--- android/app/src/main/java/com/reactnativewithcomponents/PrimaryButtonManager.java
+++ android/app/src/main/java/com/reactnativewithcomponents/PrimaryButtonManager.java
@@ -8,9 +8,11 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
-public class PrimaryButtonManager extends SimpleViewManager<Button> {
+public class PrimaryButtonManager extends SimpleViewManager<LinearLayout> {
   public static final String REACT_CLASS = "RNTPrimaryButton"; // Matches Objective-C exposed class
 
   @Override
@@ -19,24 +21,35 @@ public class PrimaryButtonManager extends SimpleViewManager<Button> {
   }
 
   @Override
-  public Button createViewInstance(ThemedReactContext context) {
+  public LinearLayout createViewInstance(ThemedReactContext context) {
+    LinearLayout wrapper = new LinearLayout(context);
     Button button = new Button(context);
     GradientDrawable shape = new GradientDrawable();
 
     shape.setCornerRadius(2);
     shape.setColor(Color.parseColor("#FA6400"));
 
-    button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, 50));
+    button.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
     button.setTextColor(Color.WHITE);
     button.setBackground(shape);
 
-    button.setMaxHeight(50);
+    wrapper.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+    wrapper.addView(button);
 
-    return button;
+    return wrapper;
   }
 
   @ReactProp(name = "title")
-  public void setTitle(Button button, @Nullable String title) {
-    button.setText(title);
+  public void setTitle(LinearLayout wrapper, @Nullable String title) {
+    // TODO: Use a child view .find() instead of loop
+    // https://stackoverflow.com/a/13887090
+    for (int i = 0; i < wrapper.getChildCount(); i += 1) {
+      View child = wrapper.getChildAt(i);
+      if (wrapper.getChildAt(i) instanceof Button) {
+        Button button = (Button) child;
+        button.setText(title);
+        break;
+      }
+    }
   }
 }
