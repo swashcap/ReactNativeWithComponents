a933c73bc42ac875ca4e4d3746548560276c0549
Cory Reed <swashcap@gmail.com>
2017-11-17 14:47:52 -0800

    fix(ios): change init to constructor

--- ios/RNTPrimaryButtonView.m
+++ ios/RNTPrimaryButtonView.m
@@ -2,30 +2,35 @@
 
 @implementation RNTPrimaryButtonView
 
-- (UIView*)init {
-  self.button = [UIButton buttonWithType:UIButtonTypeCustom];
-  self.button.backgroundColor = [UIColor colorWithRed:250.0f/255.0f
-                                           green:100.0f/255.0f
-                                           blue:0.0f/255.0f
-                                           alpha:1.0f];
-  [self.button.titleLabel setFont:[UIFont boldSystemFontOfSize:17]];
-  [self.button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-  [self.button setTitleColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5f] forState:UIControlStateHighlighted];
-  self.button.layer.cornerRadius = 2;
-  self.button.clipsToBounds = YES;
-  self.button.translatesAutoresizingMaskIntoConstraints = NO;
+- (id)initWithFrame:(CGRect)aRect
+{
+  self = [super initWithFrame:aRect];
   
-  UIView *view = [[UIView alloc] init];
-  [view addSubview:self.button];
+  if (self) {
+    self.button = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.button.backgroundColor = [UIColor colorWithRed:250.0f/255.0f
+                                             green:100.0f/255.0f
+                                             blue:0.0f/255.0f
+                                             alpha:1.0f];
+    [self.button.titleLabel setFont:[UIFont boldSystemFontOfSize:17]];
+    [self.button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.button setTitleColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5f] forState:UIControlStateHighlighted];
+    self.button.layer.cornerRadius = 2;
+    self.button.clipsToBounds = YES;
+    self.button.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    UIView *view = [[UIView alloc] init];
+    [view addSubview:self.button];
+    
+    [NSLayoutConstraint activateConstraints:@[
+      [[self.button topAnchor] constraintEqualToAnchor:[view topAnchor]],
+      [[self.button heightAnchor] constraintEqualToConstant:44],
+      [[self.button leftAnchor] constraintEqualToAnchor:[view leftAnchor]],
+      [[self.button rightAnchor] constraintEqualToAnchor:[view rightAnchor]]
+    ]];
+  }
   
-  [NSLayoutConstraint activateConstraints:@[
-    [[self.button topAnchor] constraintEqualToAnchor:[view topAnchor]],
-    [[self.button heightAnchor] constraintEqualToConstant:44],
-    [[self.button leftAnchor] constraintEqualToAnchor:[view leftAnchor]],
-    [[self.button rightAnchor] constraintEqualToAnchor:[view rightAnchor]]
-  ]];
-
-  return view;
+  return self;
 }
 
 
