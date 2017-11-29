48b61e3e6336404f400bb1242fc6f9cb4c4a083f
Cory Reed <swashcap@gmail.com>
2017-11-29 12:26:11 -0800

    fix(android): return button `onPress` events

--- android/app/src/main/java/com/reactnativewithcomponents/PrimaryButtonView.java
+++ android/app/src/main/java/com/reactnativewithcomponents/PrimaryButtonView.java
@@ -1,11 +1,11 @@
 package com.reactnativewithcomponents;
 
 import android.graphics.Typeface;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.widget.Button;
-import android.view.ViewGroup.LayoutParams;
 import android.support.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 
-public class PrimaryButtonView extends LinearLayout {
+public class PrimaryButtonView extends LinearLayout implements View.OnClickListener {
     private Button button;
 
     public PrimaryButtonView(ReactContext context) {
@@ -30,6 +30,8 @@ public class PrimaryButtonView extends LinearLayout {
         button.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
         button.setBackground(shape);
 
+        button.setOnClickListener(this);
+
         this.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         this.addView(button);
     }
@@ -44,14 +46,14 @@ public class PrimaryButtonView extends LinearLayout {
     }
 
     /**
-     * Handle button press.
+     * Handle button 'click'
      *
-     * @link https://facebook.github.io/react-native/docs/native-components-android.html#events
-     * @link https://stackoverflow.com/q/39872442
+     * @param view Button
      */
-    public void buttonPress() {
+    public void onClick(View view) {
         WritableMap event = Arguments.createMap();
         event.putString("buttonPress", "onPress");
+
         ReactContext reactContext = (ReactContext)getContext();
         reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
                 getId(),
