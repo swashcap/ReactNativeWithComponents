8e6bb7066c6c73ef91f3e8c2446f85b35c1892b5
Cory Reed <swashcap@gmail.com>
2017-11-28 16:08:15 -0800

    fix(android): correct `CardManager`'s base class

--- android/app/src/main/java/com/reactnativewithcomponents/CardManager.java
+++ android/app/src/main/java/com/reactnativewithcomponents/CardManager.java
@@ -1,9 +1,9 @@
 package com.reactnativewithcomponents;
 
-import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
 
-public class CardManager extends SimpleViewManager<CardView> {
+public class CardManager extends ViewGroupManager<CardView> {
     public static final String REACT_CLASS = "RNTCard"; // Matches Objective-C exposed class
 
     @Override
