a37259733914047c5c953a72f8b0cfa597854268
Cory Reed <swashcap@gmail.com>
2017-12-01 15:29:48 -0800

    feat(android): enable react-native child view passing to CardView

--- android/app/src/main/java/com/reactnativewithcomponents/CardManager.java
+++ android/app/src/main/java/com/reactnativewithcomponents/CardManager.java
@@ -1,5 +1,7 @@
 package com.reactnativewithcomponents;
 
+import android.view.View;
+
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 
@@ -14,5 +16,39 @@ public class CardManager extends ViewGroupManager<CardView> {
     @Override
     public CardView createViewInstance(ThemedReactContext context) {
         return new CardView(context);
+    }
+
+    /**
+     * Proxy new react-native views to the CardView.reactRoot to maintain CardView's
+     * non-react-native children.
+     */
+    @Override
+    public void addView(CardView parent, View child, int index)  {
+        parent.reactRoot.addView(child, index);
+    }
+
+    @Override
+    public int getChildCount(CardView parent) {
+        return parent.reactRoot.getChildCount();
+    }
+
+    @Override
+    public View getChildAt(CardView parent, int index) {
+        return parent.reactRoot.getChildAt(index);
+    }
+
+    @Override
+    public void removeViewAt(CardView parent, int index) {
+        parent.reactRoot.removeViewAt(index);
+    }
+
+    /**
+     * Override the superclass's method because the CardView likely needs custom layout for its
+     * react-native children.
+     * See {@link ViewGroupManager#needsCustomLayoutForChildren()}
+     */
+    @Override
+    public boolean needsCustomLayoutForChildren() {
+        return true;
     }
 }
--- android/app/src/main/java/com/reactnativewithcomponents/CardView.java
+++ android/app/src/main/java/com/reactnativewithcomponents/CardView.java
@@ -1,14 +1,45 @@
 package com.reactnativewithcomponents;
 
-import android.view.ViewGroup;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.facebook.react.bridge.ReactContext;
 
 public class CardView extends LinearLayout {
-    private ViewGroup reactRoot;
+    public LinearLayout reactRoot;
 
     public CardView(ReactContext context) {
         super(context);
+
+        this.setBackground(getCardViewBackground());
+        this.setPadding(20, 10, 20, 10);
+        this.setOrientation(LinearLayout.VERTICAL);
+
+        reactRoot = new LinearLayout(context);
+        reactRoot.setOrientation(LinearLayout.VERTICAL);
+
+        this.addView(getCardViewHeading(context, "Sample Heading"));
+        this.addView(reactRoot);
+    }
+
+    private GradientDrawable getCardViewBackground() {
+        GradientDrawable shape = new GradientDrawable();
+
+        shape.setCornerRadius(10);
+        shape.setColor(Color.MAGENTA);
+        shape.setStroke(10, Color.CYAN);
+
+        return shape;
+    }
+
+    private TextView getCardViewHeading(Context context, String text) {
+        TextView heading = new TextView(context);
+        heading.setText(text);
+        heading.setTextColor(Color.WHITE);
+
+        return heading;
     }
 }
